bfb9b2e2e86293ad77f3bf69c4d842edf070dc02
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-06-23 22:32:41 +0900

    [add]リテラルについて

--- Sample1.java
+++ Sample1.java
@@ -1,7 +1,119 @@
-class Sample1
+// class Sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("ようこそJavaへ！");
+//   }
+// }
+
+// class Sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("ようこそJavaへ！");
+//     System.out.println("Javaをはじめましょう！");
+//   }
+// }
+
+// class Sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.print("ようこそJavaへ！");
+//     System.out.print("Javaをはじめましょう！");
+//   }
+// }
+
+// class Sample3
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("A");
+//     System.out.println("ようこそJavaへ！");
+//     System.out.println(123);
+//   }
+// }
+
+// class Sample3
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("円記号を\n表示します。:");
+//     System.out.println("アポストロフィを表示します。");
+//   }
+// }
+
+// class Sample3
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("8進数101の文字は\101です。");
+//     System.out.println("16進数0061の文字は\u0061です。");
+//   }
+// }
+
+class Sample3
 {
   public static void main(String[] args)
   {
-    System.out.println("ようこそJavaへ！");
+    System.out.println(10);
+    System.out.println(010);
+    System.out.println(0x10);
+    System.out.println(0xF);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
