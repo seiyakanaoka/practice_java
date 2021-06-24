c1cd435311c26d75c5a3ac6568f338e9ec336683
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-06-24 22:04:41 +0900

    整数計算

--- Sample1.java
+++ Sample1.java
@@ -174,7 +174,32 @@
 //   }
 // }
 
+// class Sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("1+2は" + (1 + 2) + "です。");
+//     System.out.println("3*4は" + (3 * 4) + "です。");
+//   }
+// }
 
+class Sample1
+{
+  public static void main(String[] args)
+  {
+    int num1 = 2;
+    int num2 = 3;
+    int sum = num1+num2;
+
+    System.out.println("変数num1の値は" + num1 + "です。");
+    System.out.println("変数num2の値は" + num2 + "です。");
+    System.out.println("num1+num2の値は" + sum + "です。");
+
+    num1 = num1 + 1;
+
+    System.out.println("変数num1の値に1を足すと" + num1 + "です。");
+  }
+}
 
 
 
