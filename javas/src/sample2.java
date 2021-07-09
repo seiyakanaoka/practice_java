b7646065229d3d16ad44061bfeda158788993f35
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-09 14:35:05 +0900

    配列変数の定義と初期化

--- javas/src/sample2.java
+++ javas/src/sample2.java
@@ -155,3 +155,26 @@
 //   }
 // }
 
+// class sample2
+// {
+//   public static void main(String[] args)
+//   {
+//     int[] test = {80, 60, 22, 50, 75};
+
+//     for(int i = 0; i < 5; i++) {
+//       System.out.println((i + 1) + "番目の人の点数は" + test[i] + "です。");
+//     }
+//   }
+// }
+
+class sample2
+{
+  public static void main(String[] args)
+  {
+    int[] test = {80, 60, 22, 50, 75};
+
+    for(int i = 0; i < 5; i++) {
+      System.out.println((i + 1) + "番目の人の点数は" + test[i] + "です。");
+    }
+  }
+}
