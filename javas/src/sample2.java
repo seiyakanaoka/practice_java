52ad8e2c1853d0a1c96b32ac5b8ea5022f50c82d
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-13 20:42:07 +0900

    コンストラクタのオーバーライドと修飾子

--- javas/src/sample2.java
+++ javas/src/sample2.java
@@ -734,18 +734,130 @@
 //   }
 // }
 
+// class Car
+// {
+//   private int num;
+//   private double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1 = new Car();
+
+//     car1.show();
+//   }
+// }
+
+// class Car
+// {
+//   private int num;
+//   private double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+//   public Car(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
+//   }
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1 = new Car();
+//     car1.show();
+
+//     System.out.println("");
+
+//     Car car2 = new Car(1234, 20.5);
+//     car2.show();
+//   }
+// }
+
+// class Car
+// {
+//   private int num;
+//   private double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+//   public Car(int n, double g)
+//   {
+//     this();
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
+//   }
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1 = new Car();
+//     car1.show();
+
+//     System.out.println("");
+
+//     Car car2 = new Car(1234, 20.5);
+//     car2.show();
+//   }
+// }
+
 class Car
 {
   private int num;
   private double gas;
 
-  public Car()
+  private Car()
   {
     num = 0;
     gas = 0.0;
     System.out.println("車を作成しました。");
   }
-
+  public Car(int n, double g)
+  {
+    this();
+    num = n;
+    gas = g;
+    System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
+  }
   public void show()
   {
     System.out.println("車のナンバーは" + num + "です。");
@@ -757,8 +869,13 @@ class sample1
 {
   public static void main(String[] args)
   {
-    Car car1 = new Car();
+    // Car car1 = new Car();
+    // car1.show();
 
-    car1.show();
+    System.out.println("");
+
+    Car car2 = new Car(1234, 20.5);
+    car2.show();
   }
 }
+
