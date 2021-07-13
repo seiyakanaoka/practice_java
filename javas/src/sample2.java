cf987ce87aa54a9f36e85930f65915e6cb1e250a
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-13 22:01:02 +0900

    練習問題前半

--- javas/src/sample2.java
+++ javas/src/sample2.java
@@ -919,6 +919,59 @@
 //   }
 // }
 
+// class Car
+// {
+//   public static int sum = 0;
+
+//   private int num;
+//   private double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     sum++;
+//     System.out.println("車を作成しました。");
+//   }
+
+//   public void setCar(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバーを" + num + "ガソリン量を" + gas + "の車を作成しました。");
+//   }
+
+//   public static void showSum()
+//   {
+//     System.out.println("車は全部で" + sum + "台あります。");
+//   }
+
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car.showSum();
+
+//     Car car1 = new Car();
+//     car1.setCar(1234, 20.5);
+
+//     Car.showSum();
+
+//     System.out.println("");
+
+//     Car car2 = new Car();
+//     car2.setCar(4567, 30.5);
+//     Car.showSum();
+//   }
+// }
+
 class Car
 {
   public static int sum = 0;
@@ -971,4 +1024,3 @@ class sample1
     Car.showSum();
   }
 }
-
