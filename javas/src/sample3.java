a48f362b3d08b20bc6000056753258c3d671e93b
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-19 15:36:30 +0900

    クラス変数の代入

--- javas/src/sample3.java
+++ javas/src/sample3.java
@@ -152,19 +152,79 @@
 // }
 
 
-import java.io.*;
+// import java.io.*;
 
-class sample1
-{
-  public static void main(String[] args) throws IOException
-  {
-    System.out.println("整数を二つ入力してください。");
+// class sample1
+// {
+//   public static void main(String[] args) throws IOException
+//   {
+//     System.out.println("整数を二つ入力してください。");
 
-    BufferedReader br =
-      new BufferedReader(new InputStreamReader(System.in));
+//     BufferedReader br =
+//       new BufferedReader(new InputStreamReader(System.in));
 
-    int ans = (int) (Math.random() * 6) + 1;
+//     int ans = (int) (Math.random() * 6) + 1;
 
-    System.out.println( ans);
-  }
-}
+//     System.out.println( ans);
+//   }
+// }
+
+// class Car
+// {
+//   private int num;
+//   private double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+
+//   public void setCar(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
+//   }
+
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1;
+//     System.out.println("car1を宣言しました。");
+//     car1 = new Car();
+//     car1.setCar(1234, 20.5);
+
+//     Car car2;
+//     System.out.println("car2を宣言しました。");
+
+//     car2 = car1;
+//     System.out.println("car2にcar1を代入しました。");
+
+//     System.out.println("car1がさす");
+//     car1.show();
+//     System.out.println("car2がさす");
+//     car2.show();
+
+//     System.out.println("car1がさす車に変更を加えます。");
+//     car1.setCar(2345, 30.5);
+
+//     System.out.println("car1がさす");
+//     car1.show();
+//     System.out.println("car2がさす");
+//     car2.show();
+//   }
+// }
+
+
+
+
