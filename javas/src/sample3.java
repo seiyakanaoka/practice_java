7d851ead2644be54f438dfe75a4778bae215c972
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-25 21:05:17 +0900

    クラス変数の配列作成

--- javas/src/sample3.java
+++ javas/src/sample3.java
@@ -310,17 +310,70 @@
 //   }
 // }
 
+// class Car
+// {
+//   private int num;
+//   private double gas;
+//   private String name;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     name = "名無し";
+//     System.out.println("車を作成しました。");
+//   }
+
+//   public void setCar(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
+//   }
+
+//   public void setName(String nm)
+//   {
+//     name = nm;
+//     System.out.println("名前を" + name + "にしました。");
+//   }
+
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//     System.out.println("名前は" + name + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1;
+//     car1 = new Car();
+
+//     car1.show();
+
+//     int number = 1234;
+//     double gasoline = 20.5;
+//     String str = "1号車";
+
+//     car1.setCar(number, gasoline);
+//     car1.setName(str);
+
+//     car1.show();
+//   }
+// }
+
 class Car
 {
   private int num;
   private double gas;
-  private String name;
 
   public Car()
   {
     num = 0;
     gas = 0.0;
-    name = "名無し";
     System.out.println("車を作成しました。");
   }
 
@@ -331,17 +384,10 @@ class Car
     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
   }
 
-  public void setName(String nm)
-  {
-    name = nm;
-    System.out.println("名前を" + name + "にしました。");
-  }
-
   public void show()
   {
     System.out.println("車のナンバーは" + num + "です。");
     System.out.println("ガソリン量は" + gas + "です。");
-    System.out.println("名前は" + name + "です。");
   }
 }
 
@@ -349,23 +395,22 @@ class sample1
 {
   public static void main(String[] args)
   {
-    Car car1;
-    car1 = new Car();
+    Car[] cars;
+    cars = new Car[3];
 
-    car1.show();
+    for(int i = 0; i < cars.length; i++) {
+      cars[i] = new Car();
+    }
 
-    int number = 1234;
-    double gasoline = 20.5;
-    String str = "1号車";
+    cars[0].setCar(1234, 20.5);
+    cars[1].setCar(2345, 30.5);
+    cars[2].setCar(3456, 40.5);
 
-    car1.setCar(number, gasoline);
-    car1.setName(str);
-
-    car1.show();
+    for(int i = 0; i < cars.length; i++) {
+      cars[i].show();
+    }
   }
 }
-
-
 
 
 
