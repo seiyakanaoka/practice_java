9bf853d18a138707b673a5ced9b722957fc13965
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-06 14:27:09 +0900

    objectclassのメソッド１

--- javas/src/sample3.java
+++ javas/src/sample3.java
@@ -730,3 +730,70 @@ import java.nio.file.NotLinkException;
 //   }
 // }
 
+// class Car
+// {
+//   protected int num;
+//   protected double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+//   public void setCar(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
+//   }
+//   public String toString()
+//   {
+//     String str = "ナンバー:" + num + "ガソリン量:" + gas;
+//     return str;
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1 = new Car();
+//     car1.setCar(1234, 20.5);
+
+//     System.out.println(car1);
+//   }
+// }
+
+// class Car
+// {
+//   protected int num;
+//   protected double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car car1 = new Car();
+//     Car car2 = new Car();
+
+//     Car car3;
+//     car3 = car1;
+
+//     boolean bl1 = car1.equals(car2);
+//     boolean bl2 = car1.equals(car3);
+
+//     System.out.println("car1とcar2が同じか調べたところ" + bl1 + "でした。");
+//     System.out.println("car1とcar3が同じか調べたところ" + bl2 + "でした。");
+//   }
+// }
+
+
