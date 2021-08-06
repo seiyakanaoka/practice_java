c7324ae3141665026bc23ec7d457b13b90116792
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-06 13:09:56 +0900

    オーバーライド

--- javas/src/sample3.java
+++ javas/src/sample3.java
@@ -595,6 +595,61 @@ import java.nio.file.NotLinkException;
 //   }
 // }
 
+// class Car
+// {
+//   protected int num;
+//   protected double gas;
+
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+//   public void setCar(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
+//   }
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class RacingCar extends Car
+// {
+//   private int course;
+
+//   public RacingCar()
+//   {
+//     course = 0;
+//     System.out.println("レーシングカーを作成しました。");
+//   }
+//   public void setCourse(int c)
+//   {
+//     course = c;
+//     System.out.println("コース番号を" + course + "にしました。");
+//   }
+//   public void newShow()
+//   {
+//     System.out.println("レーシングカーのナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//     System.out.println("コース番号は" + course + "です。");
+//   }
+// }
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     RacingCar rccar1;
+//     rccar1 = new RacingCar();
+//     rccar1.newShow();
+//   }
+// }
+
 class Car
 {
   protected int num;
@@ -633,22 +688,25 @@ class RacingCar extends Car
     course = c;
     System.out.println("コース番号を" + course + "にしました。");
   }
-  public void newShow()
+  public void show()
   {
     System.out.println("レーシングカーのナンバーは" + num + "です。");
     System.out.println("ガソリン量は" + gas + "です。");
     System.out.println("コース番号は" + course + "です。");
   }
 }
+
 class sample1
 {
   public static void main(String[] args)
   {
     RacingCar rccar1;
     rccar1 = new RacingCar();
-    rccar1.newShow();
+
+    rccar1.setCar(1234, 20.5);
+    rccar1.setCourse(5);
+
+    rccar1.show();
   }
 }
-
-
 
