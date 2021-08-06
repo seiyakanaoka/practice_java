d9c949a232d914b76da62631fc405db4d3d53e7c
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-06 14:34:12 +0900

    objectclassのメソッド２

--- javas/src/sample3.java
+++ javas/src/sample3.java
@@ -796,4 +796,43 @@ import java.nio.file.NotLinkException;
 //   }
 // }
 
+class Car
+{
+  protected int num;
+  protected double gas;
 
+  public Car()
+  {
+    num = 0;
+    gas = 0.0;
+    System.out.println("車を作成しました。");
+  }
+}
+
+class RacingCar extends Car
+{
+  private int course;
+
+  public RacingCar()
+  {
+    course = 0;
+    System.out.println("レーシングカーを作成しました。");
+  }
+}
+
+class sample1
+{
+  public static void main(String[] args)
+  {
+    Car[] cars;
+    cars = new Car[2];
+
+    cars[0] = new Car();
+    cars[1] = new RacingCar();
+
+    for(int i = 0; i < cars.length; i++) {
+      Class cl = cars[i].getClass();
+      System.out.println((i + 1) + "番目のオブジェクトクラスは" + cl + "です。");
+    }
+  }
+}
