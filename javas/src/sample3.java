a048ffec50c6785b09bdb8b58a54dd816224c8c4
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-08 09:16:43 +0900

    Javaによるトリボナッチ配列の計算と1〜99の4乗の和

--- javas/src/sample3.java
+++ javas/src/sample3.java
@@ -837,40 +837,40 @@ import java.nio.file.NotLinkException;
 //   }
 // }
 
-class Car
-{
-  protected int num;
-  protected double gas;
+// class Car
+// {
+//   protected int num;
+//   protected double gas;
   
-  public Car()
-  {
-    num = 0;
-    gas = 0.0;
-    System.out.println("車を作成しました。");
-  }
-  public void setCar(int n, double g)
-  {
-    num = n;
-    gas = g;
-    System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
-  }
-  public String toString()
-  {
-    String str = "ナンバー" + num + "ガソリン量" + gas + "の車";
-    return str;
-  }
-}
+//   public Car()
+//   {
+//     num = 0;
+//     gas = 0.0;
+//     System.out.println("車を作成しました。");
+//   }
+//   public void setCar(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
+//   }
+//   public String toString()
+//   {
+//     String str = "ナンバー" + num + "ガソリン量" + gas + "の車";
+//     return str;
+//   }
+// }
 
-class sample1
-{
-  public static void main(String[] args)
-  {
-    Car cars = new Car();
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     Car cars = new Car();
 
-    cars.setCar(1234, 20.5);
-    System.out.println(cars + "です。");
-  }
-}
+//     cars.setCar(1234, 20.5);
+//     System.out.println(cars + "です。");
+//   }
+// }
 
 
 
