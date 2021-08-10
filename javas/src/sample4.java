6bf09d80e34cbeb223e6a2af823dcec0c2980228
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-10 17:28:48 +0900

    インターフェイスの拡張

--- javas/src/sample4.java
+++ javas/src/sample4.java
@@ -86,12 +86,72 @@
 //   }
 // }
 
+// interface iVehicle
+// {
+//   void show();
+// }
+
+// class Car implements iVehicle
+// {
+//   private int num;
+//   private double gas;
+
+//   public Car(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
+//   }
+//   public void show()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+// }
+
+// class Plane implements iVehicle
+// {
+//   private int flight;
+
+//   public Plane(int f)
+//   {
+//     flight = f;
+//     System.out.println("便" + flight + "の飛行機を作成しました。");
+//   }
+//   public void show()
+//   {
+//     System.out.println("飛行機の便は" + flight + "です。");
+//   }
+// }
+
+// class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     iVehicle[] ivc;
+//     ivc = new iVehicle[2];
+
+//     ivc[0] = new Car(1234, 20.5);
+//     ivc[1] = new Plane(232);
+
+//     for(int i = 0; i < ivc.length; i++)
+//     {
+//       ivc[i].show();
+//     }
+//   }
+// }
+
 interface iVehicle
 {
-  void show();
+  void vShow();
 }
 
-class Car implements iVehicle
+interface iMaterial
+{
+  void mShow();
+}
+
+class Car implements iVehicle, iMaterial
 {
   private int num;
   private double gas;
@@ -102,41 +162,22 @@ class Car implements iVehicle
     gas = g;
     System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
   }
-  public void show()
+  public void vShow()
   {
     System.out.println("車のナンバーは" + num + "です。");
     System.out.println("ガソリン量は" + gas + "です。");
   }
-}
-
-class Plane implements iVehicle
-{
-  private int flight;
-
-  public Plane(int f)
+  public void mShow()
   {
-    flight = f;
-    System.out.println("便" + flight + "の飛行機を作成しました。");
-  }
-  public void show()
-  {
-    System.out.println("飛行機の便は" + flight + "です。");
+    System.out.println("車の材料は鉄です。");
   }
 }
-
 class sample1
 {
-  public static void main(String[] args)
+  public static void main(String [] args)
   {
-    iVehicle[] ivc;
-    ivc = new iVehicle[2];
-
-    ivc[0] = new Car(1234, 20.5);
-    ivc[1] = new Plane(232);
-
-    for(int i = 0; i < ivc.length; i++)
-    {
-      ivc[i].show();
-    }
+    Car car1 = new Car(1234, 20.5);
+    car1.vShow();
+    car1.mShow();
   }
 }
