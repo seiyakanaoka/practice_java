c4dd6a2e717bb8c70718275fc7b62b6e31c77b5f
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-10 16:43:22 +0900

    instanceofと抽象クラスについて

--- javas/src/teamlab.java
+++ javas/src/teamlab.java
@@ -57,21 +57,21 @@
 
 // 第3問 -------------------->
 
-// class sample1
-// {
-//   public static void main(String[] args)
-//   {
-//     double ans = 0.0;
-//     int count = 0;
-//     for(double i = 1.0; ans <= 15; i++) {
-//       count += 1;
-//       ans += 1 / i;
-//     }
-//     System.out.println(ans);
-//     System.out.println(count)
-//     ;
-//   }
-// }
+class sample1
+{
+  public static void main(String[] args)
+  {
+    double ans = 0.0;
+    int count = 0;
+    for(double i = 1.0; ans <= 15; i++) {
+      count += 1;
+      ans += 1 / i;
+    }
+    System.out.println(ans);
+    System.out.println(count)
+    ;
+  }
+}
 
 // 第4問 -------------------->
 
