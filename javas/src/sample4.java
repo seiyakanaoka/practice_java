9f6feeca2094f55c19989dbeee5f6548e58fcd20
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-12 16:58:37 +0900

    ファイル分割時のコンパイル

--- javas/src/sample4.java
+++ javas/src/sample4.java
@@ -141,43 +141,44 @@
 //   }
 // }
 
-interface iVehicle
-{
-  void vShow();
-}
+// interface iVehicle
+// {
+//   void vShow();
+// }
 
-interface iMaterial
-{
-  void mShow();
-}
+// interface iMaterial
+// {
+//   void mShow();
+// }
 
-class Car implements iVehicle, iMaterial
-{
-  private int num;
-  private double gas;
+// class Car implements iVehicle, iMaterial
+// {
+//   private int num;
+//   private double gas;
 
-  public Car(int n, double g)
-  {
-    num = n;
-    gas = g;
-    System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
-  }
-  public void vShow()
-  {
-    System.out.println("車のナンバーは" + num + "です。");
-    System.out.println("ガソリン量は" + gas + "です。");
-  }
-  public void mShow()
-  {
-    System.out.println("車の材料は鉄です。");
-  }
-}
-class sample1
-{
-  public static void main(String [] args)
-  {
-    Car car1 = new Car(1234, 20.5);
-    car1.vShow();
-    car1.mShow();
-  }
-}
+//   public Car(int n, double g)
+//   {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
+//   }
+//   public void vShow()
+//   {
+//     System.out.println("車のナンバーは" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//   }
+//   public void mShow()
+//   {
+//     System.out.println("車の材料は鉄です。");
+//   }
+// }
+// class sample1
+// {
+//   public static void main(String [] args)
+//   {
+    // Car car1 = new Car(1234, 20.5);
+    // car1.vShow();
+    // car1.mShow();
+//   }
+// }
+
---
+++ javas/src/sample5.java
@@ -0,0 +1,8 @@
+class sample5
+{
+    public static void main(String[] args)
+    {
+        Car car1 = new Car();
+        car1.show();
+    }
+}
