f0050dcdcce5a6f0cfe7754935c0e1eb04c781be
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-12 17:50:10 +0900

    パッ系zー管理について

---
+++ javas/src/pb/sample6.java
@@ -0,0 +1,9 @@
+package pb;
+
+class sample6 {
+  public static void main(String[] args)
+  {
+    pc.Car car1 = new pc.Car();
+    car1.show();
+  }
+}
--- javas/src/sample5.java
+++
@@ -1,8 +0,0 @@
-class sample5
-{
-    public static void main(String[] args)
-    {
-        Car car1 = new Car();
-        car1.show();
-    }
-}
