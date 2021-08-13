07fae5619c33b035813184c36b3ab0b63ad9f6ec
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-13 20:10:28 +0900

    例外処理の出力

--- javas/src/sample4.java
+++ javas/src/sample4.java
@@ -216,6 +216,30 @@
 //     }
 // }
 
+// class sample1
+// {
+//     public static void main(String[] args)
+//     {
+//         try{
+//             int[] test;
+//             test = new int[5];
+
+//             System.out.println("test[10]に値を代入します。");
+
+//             test[10] = 80;
+//             System.out.println("test[10]に80を代入しました。");
+//         }
+//         catch(ArrayIndexOutOfBoundsException e){
+//             System.out.println("配列の要素を超えています。");
+//         }
+//         finally{
+//             System.out.println("最後に必ずこの処理をします。");
+//         }
+//         System.out.println("無事終了しました。");
+//     }
+// }
+
+
 class sample1
 {
     public static void main(String[] args)
@@ -231,9 +255,7 @@ class sample1
         }
         catch(ArrayIndexOutOfBoundsException e){
             System.out.println("配列の要素を超えています。");
-        }
-        finally{
-            System.out.println("最後に必ずこの処理をします。");
+            System.out.println(e + "という例外が発生しました。");
         }
         System.out.println("無事終了しました。");
     }
