3602b421ce8e5875a434b27c840ce34deff78c2c
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-13 21:25:34 +0900

    argsの指定

--- javas/src/sample4.java
+++ javas/src/sample4.java
@@ -401,3 +401,68 @@
 //         }
 //     }
 // }
+
+// import java.io.*;
+
+// class sample1
+// {
+//     public static void main(String[] args)
+//     {
+//         try{
+//             BufferedReader br =
+//                 new BufferedReader(new FileReader("test2.txt"));
+
+//                 int[] test = new int[8];
+//                 String str;
+
+//             for(int i = 0; i < test.length; i++) {
+//                 str = br.readLine();
+//                 test[i] = Integer.parseInt(str);
+//             }
+
+//             int max = test[0];
+//             int min = test[0];
+//             for(int i = 0; i < test.length; i++) {
+//                 if(max < test[i])
+//                     max = test[i];
+//                 if(min > test[i])
+//                     min = test[i];
+//                 System.out.println(test[i]);
+//             }
+
+//             System.out.println("最高点は" + max + "です。");
+//             System.out.println("最低点は" + min + "です。");
+
+//             br.close();
+//         }
+//         catch(IOException e) {
+//             System.out.println("入出力エラーです。");
+//         }
+//     }
+// }
+
+import java.io.*;
+
+class sample1
+{
+    public static void main(String[] args)
+    {
+        if(args.length != 1) {
+            System.out.println("ファイル名を正しく指定してください。");
+            System.exit(1);
+        }
+        try{
+            BufferedReader br =
+                new BufferedReader(new FileReader(args[0]));
+            
+            String str;
+            while((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+            br.close();
+        }
+        catch(IOException e) {
+            System.out.println("入出力エラーです。");
+        }
+    }
+}
