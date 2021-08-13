4c0106472d478035967ef58a3c98c4345d4df9f0
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-13 19:47:13 +0900

    例外処理の基本

--- javas/src/sample4.java
+++ javas/src/sample4.java
@@ -182,3 +182,36 @@
 //   }
 // }
 
+// class sample1
+// {
+//     public static void main(String[] args)
+//     {
+//         int[] test;
+//         test = new int[5];
+//         System.out.println("test[10]に値を代入します。");
+
+//         test[10] = 80;
+//         System.out.println("test[10]に80を代入しました。");
+//         System.out.println("無事終了しました。");
+//     }
+// }
+
+class sample1
+{
+    public static void main(String[] args)
+    {
+        try{
+            int[] test;
+            test = new int[5];
+
+            System.out.println("test[10]に値を代入します。");
+
+            test[10] = 80;
+            System.out.println("test[10]に80を代入しました。");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("配列の要素を超えています。");
+        }
+        System.out.println("無事終了しました。");
+    }
+}
