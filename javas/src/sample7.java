8dbc092281f31743915f7f28c63871d7e2937f7b
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-15 15:41:25 +0900

    アニメーション作成

--- javas/src/sample7.java
+++ javas/src/sample7.java
@@ -146,15 +146,58 @@
 //   }
 // }
 
+// import java.awt.*;
+// import java.awt.event.*;
+
+// class sample1 extends Frame
+// {
+//   int x = 10;
+//   int y = 10;
+
+//   public static void main(String[] args)
+//   {
+//     sample1 sm = new sample1();
+//   }
+//   public sample1()
+//   {
+//     super("サンプル");
+
+//     addWindowListener(new SampleWindowListener());
+//     addMouseListener(new SampleMouseAdapter());
+
+//     setSize(750, 600);
+//     setVisible(true);
+//   }
+//   public void paint(Graphics g)
+//   {
+//     g.setColor(Color.RED);
+//     g.fillOval(x, y, 10, 10);
+//   }
+
+//   class SampleWindowListener extends WindowAdapter
+//   {
+//     public void windowClosing(WindowEvent e)
+//     {
+//       System.exit(0);
+//     }
+//   }
+//   class SampleMouseAdapter extends MouseAdapter
+//   {
+//     public void mousePressed(MouseEvent e)
+//     {
+//       x = e.getX();
+//       y = e.getY();
+//       repaint();
+//     }
+//   }
+// }
+
 import java.awt.*;
 import java.awt.event.*;
 
-import javax.swing.event.MouseInputAdapter;
-
-class sample1 extends Frame
+class sample1 extends Frame implements Runnable
 {
-  int x = 10;
-  int y = 10;
+  int num;
 
   public static void main(String[] args)
   {
@@ -165,15 +208,30 @@ class sample1 extends Frame
     super("サンプル");
 
     addWindowListener(new SampleWindowListener());
-    addMouseListener(new SampleMouseAdapter());
+
+    Thread th;
+    th = new Thread(this);
+    th.start();
 
     setSize(750, 600);
     setVisible(true);
   }
+  public void run()
+  {
+    try{
+      for(int i = 0; i < 10; i++)
+      {
+        num = i;
+        repaint();
+        Thread.sleep(1000);
+      }
+    }
+    catch(InterruptedException e){}
+  }
   public void paint(Graphics g)
   {
-    g.setColor(Color.RED);
-    g.fillOval(x, y, 10, 10);
+    String str = num + "です。";
+    g.drawString(str, 100, 100);
   }
 
   class SampleWindowListener extends WindowAdapter
@@ -181,15 +239,6 @@ class sample1 extends Frame
     public void windowClosing(WindowEvent e)
     {
       System.exit(0);
-    }
-  }
-  class SampleMouseAdapter extends MouseAdapter
-  {
-    public void mousePressed(MouseEvent e)
-    {
-      x = e.getX();
-      y = e.getY();
-      repaint();
     }
   }
 }
