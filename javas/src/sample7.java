66db1a7e43699c5fe27942ad5c60e288dfdc62d8
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-15 15:11:19 +0900

    GUIで描画する

--- javas/src/sample7.java
+++ javas/src/sample7.java
@@ -110,3 +110,86 @@
 //   }
 // }
 
+// import java.awt.*;
+// import java.awt.event.*;
+
+// class sample1 extends Frame
+// {
+//   Image im;
+
+//   public static void main(String[] args)
+//   {
+//     sample1 sm = new sample1();
+//   }
+//   public sample1()
+//   {
+//     super("サンプル");
+
+//     Toolkit tk = getToolkit();
+//     im = tk.getImage("Image.jpg");
+
+//     addWindowListener(new SampleWindowListener());
+
+//     setSize(750, 600);
+//     setVisible(true);
+//   }
+//   public void paint(Graphics g)
+//   {
+//     g.drawImage(im, 100, 100, this);
+//   }
+//   class SampleWindowListener extends WindowAdapter
+//   {
+//     public void windowClosing(WindowEvent e)
+//     {
+//       System.exit(0);
+//     }
+//   }
+// }
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.event.MouseInputAdapter;
+
+class sample1 extends Frame
+{
+  int x = 10;
+  int y = 10;
+
+  public static void main(String[] args)
+  {
+    sample1 sm = new sample1();
+  }
+  public sample1()
+  {
+    super("サンプル");
+
+    addWindowListener(new SampleWindowListener());
+    addMouseListener(new SampleMouseAdapter());
+
+    setSize(750, 600);
+    setVisible(true);
+  }
+  public void paint(Graphics g)
+  {
+    g.setColor(Color.RED);
+    g.fillOval(x, y, 10, 10);
+  }
+
+  class SampleWindowListener extends WindowAdapter
+  {
+    public void windowClosing(WindowEvent e)
+    {
+      System.exit(0);
+    }
+  }
+  class SampleMouseAdapter extends MouseAdapter
+  {
+    public void mousePressed(MouseEvent e)
+    {
+      x = e.getX();
+      y = e.getY();
+      repaint();
+    }
+  }
+}
