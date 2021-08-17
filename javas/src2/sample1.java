72f97e6d6191fe89b38d70a740e76571b9d4ad20
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-08-17 20:28:21 +0900

    OpenJFX環境の構築

---
+++ javas/src2/sample1.java
@@ -0,0 +1,32 @@
+// public class sample1
+// {
+//   public static void main(String[] args)
+//   {
+//     System.out.println("ようこそアプリケーションへ！");
+//   }
+// }
+
+import com.apple.eawt.Application;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class sample1 extends Application
+{
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
+  public void start(Stage stage)throws Exception
+  {
+    BorderPane bp = new BorderPane();
+    Scene sc = new Scene(bp, 300, 200);
+
+    stage.setScene(sc);
+    stage.setTitle("サンプル");
+    stage.show();
+  }
+}
