d1a7d0988a2203a1ca615de5e7bf8e1e8bd52514
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-12 23:00:53 +0900

    [update]static

--- javas/src2/sample2.java
+++ javas/src2/sample2.java
@@ -1,17 +1,26 @@
 class Car {
+  public static int sum = 0;
+
   private int num;
   private double gas;
 
   public Car() {
     num = 0;
     gas = 0.0;
+    sum++;
     System.out.println("車を作成しました。");
   }
+
   public void setCar(int n, double g) {
     num = n;
     gas = g;
     System.out.println("ナンバーを" + num + "ガソリン量を" + gas + "にしました。");
   }
+
+  public static void showSum() {
+    System.out.println("車は全部で" + sum + "台あります。");
+  }
+
   public void show() {
     System.out.println("車のナンバーは" + num + "です。");
     System.out.println("ガソリン量は" + gas + "です。");
@@ -20,12 +29,22 @@ class Car {
 
 class sample1 {
   public static void main(String[] args) {
+    Car.showSum();
+
+    System.out.println();
+
     Car car1 = new Car();
     car1.setCar(1234, 20.5);
     car1.show();
 
+    Car.showSum();
+
+    System.out.println();
+
     Car car2 = new Car();
     car2.setCar(4567, 30.5);
     car2.show();
+
+    Car.showSum();
   }
 }
