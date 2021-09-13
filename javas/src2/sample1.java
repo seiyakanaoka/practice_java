733d939faf4f2473196d5b05c505929e88c6bc44
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-13 22:31:23 +0900

    [add]オブジェクトの配列

--- javas/src2/sample1.java
+++ javas/src2/sample1.java
@@ -141,3 +141,43 @@
 //     car1.show();
 //   }
 // }
+
+class Car {
+  private int num;
+  private double gas;
+
+  public Car() {
+    num = 0;
+    gas = 0.0;
+    System.out.println("車を作成しました。");
+  }
+  public void setCar(int n, double g) {
+    num = n;
+    gas = g;
+    System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
+  }
+  public void show() {
+    System.out.println("車のナンバーは" + num + "です。");
+    System.out.println("ガソリン量は" + gas + "です。");
+  }
+}
+
+class sample1 {
+  public static void main(String[] args) {
+    Car[] cars;
+    cars = new Car[3];
+
+    System.out.println();
+
+    for(int i = 0; i < cars.length; i++) {
+      cars[i] = new Car();
+    }
+    cars[0].setCar(1234, 20.5);
+    cars[1].setCar(2345, 30.5);
+    cars[2].setCar(3456, 40.5);
+
+    for(int i = 0; i < cars.length; i++) {
+      cars[i].show();
+    }
+  }
+}
