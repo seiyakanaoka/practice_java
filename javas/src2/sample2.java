69737ca146747bf83297a90f2360542423795cd1
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-17 14:29:08 +0900

    1~100までのFizzBuzz作成

--- javas/src2/sample2.java
+++ javas/src2/sample2.java
@@ -49,28 +49,50 @@
 //   }
 // }
 
-import java.io.*;
+// import java.io.*;
 
-class sample1 {
-  public static void main(String[] args) throws IOException {
-    System.out.println("これより、FizzBuzzゲームを開始します。");
-    System.out.println("好きな数字を入力してください。その値によって、出力が変わります。");
+// class sample1 {
+//   public static void main(String[] args) throws IOException {
+//     System.out.println("これより、FizzBuzzゲームを開始します。");
+//     System.out.println("好きな数字を入力してください。その値によって、出力が変わります。");
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String str = br.readLine();
-    int num = Integer.parseInt(str);
+//     String str = br.readLine();
+//     int num = Integer.parseInt(str);
 
-    if (num % 3 == 0 && num % 5 == 0) {
-      System.out.println("FizzBuzz!");
-    } else if (num % 3 == 0) {
-      System.out.println("Fizz!");
-    } else if (num % 5 == 0) {
-      System.out.println("Buzz!");
-    } else {
-      System.out.println(num);
-    }
+//     if (num % 3 == 0 && num % 5 == 0) {
+//       System.out.println("FizzBuzz!");
+//     } else if (num % 3 == 0) {
+//       System.out.println("Fizz!");
+//     } else if (num % 5 == 0) {
+//       System.out.println("Buzz!");
+//     } else {
+//       System.out.println(num);
+//     }
 
-    System.out.println("FizzBuzzゲームを終了します。");
-  }
-}
+//     System.out.println("FizzBuzzゲームを終了します。");
+//   }
+// }
+
+// class sample1 {
+//   public static void main(String[] args) {
+//     System.out.println("これより、FizzBuzzゲームを開始します。");
+//     System.out.println("1~100の数字の出力結果を表示します。");
+
+//     for(int i = 1; i < 101; i++) {
+//       if (i % 3 == 0 && i % 5 == 0) {
+//         System.out.println("FizzBuzz!");
+//       } else if (i % 3 == 0) {
+//         System.out.println("Fizz!");
+//       } else if (i % 5 == 0) {
+//         System.out.println("Buzz!");
+//       } else {
+//         System.out.println(i);
+//       }
+//     }
+
+//     System.out.println("FizzBuzzゲームを終了します。");
+//   }
+// }
+
