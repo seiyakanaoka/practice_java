3ce8495571559b1ae22aa0c054070cc24cc4ebb7
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-28 10:07:50 +0900

    [練習]1〜100までの素数判定

--- javas/src2/sample2.java
+++ javas/src2/sample2.java
@@ -234,66 +234,100 @@
 //   }
 // }
 
-abstract class Vehicle {
-  protected int speed;
+// abstract class Vehicle {
+//   protected int speed;
 
-  public void setSpeed(int s) {
-    speed = s;
-    System.out.println("速度を" + speed + "にしました。");
-  }
+//   public void setSpeed(int s) {
+//     speed = s;
+//     System.out.println("速度を" + speed + "にしました。");
+//   }
 
-  abstract void show();
-}
+//   abstract void show();
+// }
 
-class Car extends Vehicle {
-  private int num;
-  private double gas;
+// class Car extends Vehicle {
+//   private int num;
+//   private double gas;
 
-  public Car(int n, double g) {
-    num = n;
-    gas = g;
-    System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
-  }
+//   public Car(int n, double g) {
+//     num = n;
+//     gas = g;
+//     System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を作成しました。");
+//   }
 
-  public void show() {
-    System.out.println("車のナンバー" + num + "です。");
-    System.out.println("ガソリン量は" + gas + "です。");
-    System.out.println("速度は" + speed + "です。");
-  }
-}
+//   public void show() {
+//     System.out.println("車のナンバー" + num + "です。");
+//     System.out.println("ガソリン量は" + gas + "です。");
+//     System.out.println("速度は" + speed + "です。");
+//   }
+// }
 
-class Plane extends Vehicle {
-  private int flight;
+// class Plane extends Vehicle {
+//   private int flight;
 
-  public Plane(int f) {
-    flight = f;
-    System.out.println("便" + flight + "の飛行機を作成しました。");
-  }
+//   public Plane(int f) {
+//     flight = f;
+//     System.out.println("便" + flight + "の飛行機を作成しました。");
+//   }
 
-  public void show() {
-    System.out.println("飛行機の便は" + flight + "です。");
-    System.out.println("速度は" + speed + "です。");
-  }
-}
+//   public void show() {
+//     System.out.println("飛行機の便は" + flight + "です。");
+//     System.out.println("速度は" + speed + "です。");
+//   }
+// }
 
-class sample1 {
-  public static void main(String[] args) {
-    Vehicle[] vc;
-    vc = new Vehicle[2];
+// class sample1 {
+//   public static void main(String[] args) {
+//     Vehicle[] vc;
+//     vc = new Vehicle[2];
 
-    vc[0] = new Car(1234, 20.5);
-    vc[0].setSpeed(60);
+//     vc[0] = new Car(1234, 20.5);
+//     vc[0].setSpeed(60);
 
-    System.out.println();
+//     System.out.println();
 
-    vc[1] = new Plane(232);
-    vc[1].setSpeed(500);
+//     vc[1] = new Plane(232);
+//     vc[1].setSpeed(500);
 
-    System.out.println();
+//     System.out.println();
 
-    for (int i = 0; i < vc.length; i++) {
-      vc[i].show();
-      System.out.println();
-    }
-  }
-}
+//     for (int i = 0; i < vc.length; i++) {
+//       vc[i].show();
+//       System.out.println();
+//     }
+//   }
+// }
+
+// class Sosu {
+
+//   private int num = 0;
+
+//   public void so() {
+//     System.out.println("1〜100の素数判定をします。");
+
+//     for (int i = 1; i <= 100; i++) {
+//       if (i == 1) {
+//         continue;
+//       }
+//       num = 0;
+//       for (int x = 1; x <= Math.sqrt(i); x++) {
+//         if (i % x == 0) {
+//           num += 1;
+//         }
+//       }
+//       if (num == 1) {
+//         System.out.println("「" + i + "」" + "は素数です。");
+//       } else {
+//         System.out.println("「" + i + "」" + "は素数ではありません。");
+//       }
+//     }
+//   }
+// }
+
+// class sample1 {
+//   public static void main(String[] args) {
+//     Sosu sosu = new Sosu();
+
+//     sosu.so();
+//   }
+// }
