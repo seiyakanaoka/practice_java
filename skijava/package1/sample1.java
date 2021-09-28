ba65ae0bbf30da2cc71bf0cdbce025064bd709ea
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-28 16:31:39 +0900

    スッキリわかるJava実践編：第１章　練習問題

--- skijava/package1/sample1.java
+++ skijava/package1/sample1.java
@@ -1,3 +1,5 @@
+// import java.util.ArrayList;
+
 // class Hero
 // {
 //   private String name;
@@ -38,3 +40,52 @@
 
 //   }
 // }
+
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+class sample1 {
+  public static void main(String[] args) throws Exception {
+    SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd");
+    List<Book> books = new ArrayList<>();
+
+    Book book1 = new Book();
+    Book book2 = new Book();
+    Book book3 = new Book();
+
+    book1.setTitle("Java入門");
+    System.out.println("タイトルを" + book1.getTitle() + "にしました。");
+    book1.setPublishDate(f.parse("2011/10/07"));
+    System.out.println("発行日を" + book1.getPublishData() + "にしました。");
+    book1.setComment("スッキリわかる");
+    System.out.println("コメントを" + book1.getComment() + "にしました。");
+
+    System.out.println();
+
+    book2.setTitle("Python入門");
+    System.out.println("タイトルを" + book2.getTitle() + "にしました。");
+    book2.setPublishDate(f.parse("2019/06/11"));
+    System.out.println("発行日を" + book2.getPublishData() + "にしました。");
+    book2.setComment("カレーが食べたくなる");
+    System.out.println("コメントを" + book2.getComment() + "にしました。");
+
+    System.out.println();
+
+    book3.setTitle("C言語入門");
+    System.out.println("タイトルを" + book3.getTitle() + "にしました。");
+    book3.setPublishDate(f.parse("2018/06/21"));
+    System.out.println("発行日を" + book3.getPublishData() + "にしました。");
+    book3.setComment("ポインタも自由自在");
+    System.out.println("コメントを" + book3.getComment() + "にしました。");
+
+    System.out.println();
+
+    books.add(book1);
+    books.add(book2);
+    books.add(book3);
+
+    Collections.sort(books, new TitleComparator());
+
+    System.out.println(books.toString());
+  }
+}
