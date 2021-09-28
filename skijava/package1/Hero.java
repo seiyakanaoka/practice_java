ae00c18e0f87c9c377aea740cc7c4cd6351470a9
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-28 17:26:09 +0900

    equalsメソッドのオーバーライドによる、可変配列のremove実行

--- skijava/package1/Hero.java
+++ skijava/package1/Hero.java
@@ -9,26 +9,52 @@ public class Hero {
     System.out.println("フィールドを初期化しました。");
   }
 
-  public String toString() {
-    return "勇者(名前=" + this.name + "/HP=" + this.hp + "/MP=" + this.mp + ")" ;
+  public boolean equals(Object o) {
+    if (o == this)
+      return true;
+    if (o == null)
+      return false;
+    if (!(o instanceof Account))
+      return false;
+    Hero h = (Hero) o;
+    if (!this.name.trim().equals(h.name.trim())) {
+      return false;
+    }
+    if (!(this.hp == h.hp)) {
+      return false;
+    }
+    if (!(this.mp == h.mp)) {
+      return false;
+    }
+    return true;
   }
+
+  public String toString() {
+    return "勇者(名前=" + this.name + "/HP=" + this.hp + "/MP=" + this.mp + ")";
+  }
+
   public String getName() {
     return this.name;
   }
+
   public void setName(String n) {
     this.name = n;
     System.out.println(this.name + "を設定しました。");
   }
+
   public int getHp() {
     return this.hp;
   }
+
   public void setHp(int h) {
     this.hp = h;
     System.out.println(this.hp + "を設定しました。");
   }
+
   public int getMp() {
     return this.mp;
   }
+
   public void setMp(int m) {
     this.mp = m;
     System.out.println(this.mp + "を設定しました。");
--- skijava/package1/sample1.java
+++ skijava/package1/sample1.java
@@ -110,23 +110,83 @@
 //   }
 // }
 
+// import java.util.*;
+
+// class sample1 {
+//   public static void main(String[] args) {
+//     List<Account> ac = new ArrayList<>();
+//     Account a1 = new Account();
+//     a1.accountNo = "001";
+
+//     // (1)自分自身が引数として渡された場合、無条件でtrueを返す。
+//     System.out.println(a1.equals(a1));
+//     // (2)nullが引数として渡された場合、無条件でfalseを返す。
+//     System.out.println(a1.equals(null));
+//     // (3)型が異なるならば、falseを返す。
+//     String s1 = "";
+//     System.out.println(a1.equals(s1));
+//     // (4)型が同じなら、2つのインスタンスが持つフィールド同士を比較して等価か判定する
+//     Account a2 = new Account();
+//     a2.accountNo = "002";
+//     System.out.println(a1.equals(a2));
+//     a2.accountNo = "  001";
+//     System.out.println(a1.equals(a2));
+//   }
+// }
+
+import java.util.*;
+
 class sample1 {
   public static void main(String[] args) {
-    Account a1 = new Account();
-    a1.accountNo = "001";
+    List<Hero> heros = new ArrayList<>();
+    Hero hero1 = new Hero();
+    Hero hero2 = new Hero();
+    Hero hero3 = new Hero();
 
-    // (1)自分自身が引数として渡された場合、無条件でtrueを返す。
-    System.out.println(a1.equals(a1));
-    // (2)nullが引数として渡された場合、無条件でfalseを返す。
-    System.out.println(a1.equals(null));
-    // (3)型が異なるならば、falseを返す。
-    String s1 = "";
-    System.out.println(a1.equals(s1));
-    // (4)型が同じなら、2つのインスタンスが持つフィールド同士を比較して等価か判定する
-    Account a2 = new Account();
-    a2.accountNo = "002";
-    System.out.println(a1.equals(a2));
-    a2.accountNo = "  001";
-    System.out.println(a1.equals(a2));
+    hero1.setName("誠也1");
+    hero1.setHp(100);
+    hero1.setMp(100);
+
+    System.out.println();
+
+    System.out.println(hero1.getName());
+    System.out.println(hero1.getHp());
+    System.out.println(hero1.getMp());
+
+    System.out.println();
+
+    hero2.setName("誠也2");
+    hero2.setHp(200);
+    hero2.setMp(200);
+
+    System.out.println();
+
+    System.out.println(hero2.getName());
+    System.out.println(hero2.getHp());
+    System.out.println(hero2.getMp());
+
+    System.out.println();
+
+    hero3.setName("誠也3");
+    hero3.setHp(300);
+    hero3.setMp(300);
+
+    System.out.println();
+
+    System.out.println(hero3.getName());
+    System.out.println(hero3.getHp());
+    System.out.println(hero3.getMp());
+
+    System.out.println();
+
+    heros.add(hero1);
+    heros.add(hero2);
+    heros.add(hero3);
+
+    System.out.println(heros.toString());
+
+    heros.remove(hero1);
+
+    System.out.println(heros.toString());
   }
 }
