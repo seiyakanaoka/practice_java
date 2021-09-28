8502b098ed7b99752ae730494421fa60a9b0dadb
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-28 09:54:21 +0900

    equalsメソッド

--- skijava/package1/sample1.java
+++ skijava/package1/sample1.java
@@ -1,18 +1,40 @@
-class Hero
-{
-  private String name;
-  private int hp, mp;
+// class Hero
+// {
+//   private String name;
+//   private int hp, mp;
 
-  public String toString() {
-    return "勇者(名前=" + this.name + "/HP=" + this.hp + "/MP=" + this.mp + ")";
-  }
-}
+//   public String toString() {
+//     return "勇者(名前=" + this.name + "/HP=" + this.hp + "/MP=" + this.mp + ")";
+//   }
+// }
 
-public class sample1
-{
-  public static void main(String[] args) 
-  {
-    Hero hero = new Hero();
-    System.out.println(hero.toString());
-  }
-}
+// public class sample1
+// {
+//   public static void main(String[] args) 
+//   {
+//     Hero hero = new Hero();
+//     System.out.println(hero.toString());
+//   }
+// }
+
+// class Account {
+//   String accountNo;
+
+//   public boolean equals(Object o) {
+//     if (o == this) return true;
+//     if (o == null) return false;
+//     if (!(o instanceof Account)) return false;
+//     Account r = (Account)o;
+//     if (!this.accountNo.trim().equals(r.accountNo.trim())) {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
+
+// class sample1 {
+//   public static void main(String[] args) {
+//     Account ac = new Account();
+
+//   }
+// }
