23c95de875e9fd44a19220c816164da2a59342d5
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-09-28 17:13:02 +0900

    equalsメソッドのオーバーライド

--- skijava/package1/sample1.java
+++ skijava/package1/sample1.java
@@ -90,22 +90,43 @@
 //   }
 // }
 
+// class sample1 {
+//   public static void main(String[] args) {
+//     Hero hero = new Hero();
+
+//     hero.setName("誠也");
+//     hero.setHp(100);
+//     hero.setMp(100);
+
+//     System.out.println();
+
+//     System.out.println(hero.getName());
+//     System.out.println(hero.getHp());
+//     System.out.println(hero.getMp());
+
+//     System.out.println();
+
+//     System.out.println(hero.toString());
+//   }
+// }
+
 class sample1 {
   public static void main(String[] args) {
-    Hero hero = new Hero();
+    Account a1 = new Account();
+    a1.accountNo = "001";
 
-    hero.setName("誠也");
-    hero.setHp(100);
-    hero.setMp(100);
-
-    System.out.println();
-
-    System.out.println(hero.getName());
-    System.out.println(hero.getHp());
-    System.out.println(hero.getMp());
-
-    System.out.println();
-
-    System.out.println(hero.toString());
+    // (1)自分自身が引数として渡された場合、無条件でtrueを返す。
+    System.out.println(a1.equals(a1));
+    // (2)nullが引数として渡された場合、無条件でfalseを返す。
+    System.out.println(a1.equals(null));
+    // (3)型が異なるならば、falseを返す。
+    String s1 = "";
+    System.out.println(a1.equals(s1));
+    // (4)型が同じなら、2つのインスタンスが持つフィールド同士を比較して等価か判定する
+    Account a2 = new Account();
+    a2.accountNo = "002";
+    System.out.println(a1.equals(a2));
+    a2.accountNo = "  001";
+    System.out.println(a1.equals(a2));
   }
 }
