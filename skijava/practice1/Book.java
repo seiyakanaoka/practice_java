de04d2abe74681be8d724e726f56316b2de1f6b9
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-10-04 08:37:24 +0900

    クラスパス

--- skijava/practice1/Book.java
+++ skijava/practice1/Book.java
@@ -1,66 +1,66 @@
-import java.util.*;
+// import java.util.*;
 
-public class Book implements Comparable<Book>, Cloneable {
-  private String title;
-  private Date publishDate;
-  private String comment;
+// public class Book implements Comparable<Book>, Cloneable {
+//   private String title;
+//   private Date publishDate;
+//   private String comment;
 
-  public String toString() {
-    return "好きな本のタイトルは" + this.title + "で、発効日は" + this.publishDate + "で、コメントは" + this.comment + "です。";
-  }
+//   public String toString() {
+//     return "好きな本のタイトルは" + this.title + "で、発効日は" + this.publishDate + "で、コメントは" + this.comment + "です。";
+//   }
 
-  public boolean equals(Object o) {
-    if (o == this) return true;
-    if (o == null) return false;
-    if (!(o instanceof Book)) return false;
-    Book r = (Book)o;
-    if (!this.title.trim().equals(r.title.trim())) {
-      return false;
-    }
-    if (!this.comment.trim().equals(r.comment.trim())) {
-      return false;
-    }
-    if (!this.publishDate.equals(r.publishDate)) {
-      return false;
-    }
-    return true;
-  }
+//   public boolean equals(Object o) {
+//     if (o == this) return true;
+//     if (o == null) return false;
+//     if (!(o instanceof Book)) return false;
+//     Book r = (Book)o;
+//     if (!this.title.trim().equals(r.title.trim())) {
+//       return false;
+//     }
+//     if (!this.comment.trim().equals(r.comment.trim())) {
+//       return false;
+//     }
+//     if (!this.publishDate.equals(r.publishDate)) {
+//       return false;
+//     }
+//     return true;
+//   }
 
-  public int hashCode() {
-    return Objects.hash(this.title, this.publishDate, this.comment);
-  }
+//   public int hashCode() {
+//     return Objects.hash(this.title, this.publishDate, this.comment);
+//   }
 
-  public int compareTo(Book book) {
-    return this.publishDate.compareTo(book.publishDate);
-  }
+//   public int compareTo(Book book) {
+//     return this.publishDate.compareTo(book.publishDate);
+//   }
 
-  public Book clone() {
-    Book result = new Book();
-    result.title = this.title;
-    result.publishDate = (Date)this.publishDate.clone();
-    result.comment = this.comment;
-    return result;
-  }
+//   public Book clone() {
+//     Book result = new Book();
+//     result.title = this.title;
+//     result.publishDate = (Date)this.publishDate.clone();
+//     result.comment = this.comment;
+//     return result;
+//   }
 
-  public void setTitle(String t) {
-    this.title = t;
-  }
-  public void setPublishDate(Date d) {
-    this.publishDate = d;
-  }
-  public void setComment(String c) {
-    this.comment = c;
-  }
-  public String getTitle() {
-    System.out.println(this.title + "をタイトルに設定しています。");
-    return this.title;
-  }
-  public Date getPublishDate() {
-    System.out.println(this.publishDate+ "を発行日に設定しています。");
-    return this.publishDate;
-  }
-  public String getComment() {
-    System.out.println(this.comment + "をコメントに設定しています。");
-    return this.comment;
-  }
-}
+//   public void setTitle(String t) {
+//     this.title = t;
+//   }
+//   public void setPublishDate(Date d) {
+//     this.publishDate = d;
+//   }
+//   public void setComment(String c) {
+//     this.comment = c;
+//   }
+//   public String getTitle() {
+//     System.out.println(this.title + "をタイトルに設定しています。");
+//     return this.title;
+//   }
+//   public Date getPublishDate() {
+//     System.out.println(this.publishDate+ "を発行日に設定しています。");
+//     return this.publishDate;
+//   }
+//   public String getComment() {
+//     System.out.println(this.comment + "をコメントに設定しています。");
+//     return this.comment;
+//   }
+// }
--- skijava/practice1/Main.java
+++ skijava/practice1/Main.java
@@ -1,33 +1,49 @@
-import java.util.*;
-import java.text.SimpleDateFormat;
+// import java.util.*;
+// import java.text.SimpleDateFormat;
+
+// public class Main {
+//   public static void main(String[] args) throws Exception {
+//     SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd");
+
+//     Book book1 = new Book();
+
+//     book1.setTitle("こんにちわ");
+//     book1.setPublishDate(f.parse("2017/01/01"));
+//     book1.setComment("こんにちわ");
+//     book1.getTitle();
+//     book1.getPublishDate();
+//     book1.getComment();
+
+//     Book book2 = book1.clone();
+
+//     book1.setTitle("こんにち");
+//     book1.setPublishDate(f.parse("2017/01/11"));
+//     book1.setComment("こんにち");
+//     book1.getTitle();
+//     book1.getPublishDate();
+//     book1.getComment();
+
+//     book2.getTitle();
+//     book2.getPublishDate();
+//     book2.getComment();
+
+//     System.out.println(book1.toString());
+//     System.out.println(book2.toString());
+//   }
+// }
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd");
+  public static void main(String[] args) {
+    Bank b1 = new Bank();
+    Bank b2 = new Bank();
 
-    Book book1 = new Book();
+    b1.name = "ミナト";
+    b1.adress = "東京都";
+    b2.name = "ミナト";
+    b2.adress = "東京都";
+    System.out.println(b1.equals(b2));
 
-    book1.setTitle("こんにちわ");
-    book1.setPublishDate(f.parse("2017/01/01"));
-    book1.setComment("こんにちわ");
-    book1.getTitle();
-    book1.getPublishDate();
-    book1.getComment();
-
-    Book book2 = book1.clone();
-
-    book1.setTitle("こんにち");
-    book1.setPublishDate(f.parse("2017/01/11"));
-    book1.setComment("こんにち");
-    book1.getTitle();
-    book1.getPublishDate();
-    book1.getComment();
-
-    book2.getTitle();
-    book2.getPublishDate();
-    book2.getComment();
-    
-    System.out.println(book1.toString());
-    System.out.println(book2.toString());
+    b2.adress = "埼玉県";
+    System.out.println(b1.equals(b2));
   }
 }
